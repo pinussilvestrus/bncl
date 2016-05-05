a2a7fbb379c87a9538231f06e7d1fb47da26d817
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2016-05-05 12:06:01 +0200

    create gradle project

---
+++ src/main/java/de/niklaskiefer/bpmnncl/MainApplication.java
@@ -0,0 +1,10 @@
+package main.java.de.niklaskiefer.bpmnncl;
+
+/**
+ * @author Niklas Kiefer
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
