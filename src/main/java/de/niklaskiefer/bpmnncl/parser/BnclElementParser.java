1e8c3f32cea6ca3a4d707c956e6e46cc8bc4a1ef
Niklas Kiefer <niklas.kiefer@medx.net>
2016-06-02 16:03:44 +0200

    close #1

--- src/main/java/de/niklaskiefer/bpmnncl/parser/BnclElementParser.java
+++ src/main/java/de/niklaskiefer/bpmnncl/parser/BnclElementParser.java
@@ -2,6 +2,7 @@ package de.niklaskiefer.bpmnncl.parser;
 
 import de.niklaskiefer.bpmnncl.BPMNModelBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,37 +15,81 @@ import java.util.Map;
 public class BnclElementParser extends AbstractBnclParser {
 
     // attribute types
-    protected static final String ATTRIBUTE_NAME = "called";
-    protected static final String ATTRIBUTE_ID = "signed";
-
-    private static final String NO_ID_EXCEPTION = "This element has not id! Please add a \'signed\' attribute";
+    protected List<AttributeElement> attributeTypes = new ArrayList<>();
+    private static final String NO_ID_EXCEPTION = "This element has no id! Please add a \'signed\' attribute";
 
     protected BPMNModelBuilder builder;
 
     public BnclElementParser(BPMNModelBuilder builder) {
         this.builder = builder;
+        initAttributeTypes();
     }
 
     protected Map<String, String> parseAttributes(List<String> components) throws Exception {
         Map<String, String> attributes = new HashMap<>();
-        for (int i = 0; i < components.size(); i++) {
+        int i = 0;
+        while (i < components.size()) {
             String word = components.get(i);
-            switch (word.toLowerCase()) {
-                case ATTRIBUTE_NAME:
-                    attributes.put("name", components.get(i + 1));
-                    break;
-                case ATTRIBUTE_ID:
-                    attributes.put("id", components.get(i + 1));
-                    break;
-                default:
-                    break;
+            AttributeElement match = findAttributeFromWord(word);
+            if (match != null) {
+                i++;
+                String value = "";
+                StringBuilder valueBuffer = new StringBuilder();
+                // add values to attributes value as long another attribute type was found
+                while (i < components.size() && findAttributeFromWord(components.get(i)) == null) {
+                    valueBuffer.append(components.get(i)).append(" ");
+                    i++;
+                }
+
+                value = valueBuffer.toString();
+
+                if (value.lastIndexOf(' ') == value.length() - 1 && value.length() > 0) {
+                    value = value.substring(0, value.length() - 1);
+                }
+
+                attributes.put(match.getId(), value);
+                i--; // go back to start with another attribute
             }
+            i++;
         }
+
 
         if (!attributes.containsKey("id")) {
             throw new Exception(NO_ID_EXCEPTION);
         }
 
         return attributes;
+    }
+
+    private AttributeElement findAttributeFromWord(String word) {
+        for (AttributeElement attributeType : attributeTypes) {
+            if (attributeType.getBncl().equals(word)) {
+                return attributeType;
+            }
+        }
+        return null;
+    }
+
+    private void initAttributeTypes() {
+        attributeTypes.add(new AttributeElement("id", "signed"));
+        attributeTypes.add(new AttributeElement("name", "called"));
+    }
+
+    private static class AttributeElement {
+        private String id;
+        private String bncl;
+
+        public AttributeElement(String id, String bncl) {
+            this.id = id;
+            this.bncl = bncl;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getBncl() {
+            return bncl;
+        }
     }
 }
