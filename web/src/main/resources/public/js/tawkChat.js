c1cadfd97a6437b39f7343446ef32daf30daace4
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-09-19 17:54:00 +0200

    WEB: #37 add tawk.to chatbox

---
+++ web/src/main/resources/public/js/tawkChat.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/57dfe308a4d10d675d90743a/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+})();
