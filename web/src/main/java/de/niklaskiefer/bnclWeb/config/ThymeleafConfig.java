b7b9ec9fa719450fc958f610089fe9e2660c1e9e
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-02-09 15:21:28 +0100

    chore(spring): fix integrations

--- web/src/main/java/de/niklaskiefer/bnclWeb/config/ThymeleafConfig.java
+++ web/src/main/java/de/niklaskiefer/bnclWeb/config/ThymeleafConfig.java
@@ -2,9 +2,9 @@ package de.niklaskiefer.bnclWeb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 public class ThymeleafConfig {
